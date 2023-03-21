4e857e69fe7f0637640a82f3d36051f6e6844a3a
Bernardo Nunes <bmn02@outlook.com>
2023-03-21 08:23:16 -0300

    feat: fifth class chapter five done

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -28,7 +28,6 @@ interface Cycle {
   id: string
   task: string
   minutesAmount: number
-  // isActive: boolean
   startDate: Date
 }
 
@@ -48,12 +47,20 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: number
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
+
+      console.log(interval)
+    }
+
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -69,6 +76,7 @@ export function Home() {
 
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
+    setAmountSecondsPassed(0)
 
     reset()
   }
@@ -81,6 +89,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task')
   const isSubmitDisabled = !task
